acb3b47117d1b00a482383d0df630109f78edda2
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-02-12 18:40:41 -0300

    implementação do case na calculadora simples

--- calculadora_simples.rb
+++ calculadora_simples.rb
@@ -2,9 +2,32 @@ print "Digite o primeiro número inteiro: "
 number1 = gets.chomp.to_i
 print "Digite o segundo número inteiro: "
 number2 = gets.chomp.to_i
-addition = number1 + number2
-subtraction = number1 - number2
-division = number1 / number2
-puts "O resultado da adição entre os dois números é #{addition}"
-puts "O resultado da subitração entre os dois números é #{subtraction}"
-puts "O resultado da divisão entre os dois números é #{division}"
+
+puts "\nEscolha uma operação:\n"
+puts "Soma\t\t->\t+"
+puts "Subitração\t->\t-"
+puts "Multiplicação\t->\t*"
+puts "Divisão\t\t->\t/"
+
+choice = gets.chomp
+
+case choice
+when '+'
+    addition = number1 + number2
+    puts "O resultado da soma é: #{addition}"
+when '-'
+    subtraction = number1 - number2
+    puts "O resultado da subtração é: #{subtraction}"
+when '*'
+    times = number1 * number2
+    puts "O resultado da multiplicação é: #{times}"
+when '/'
+    if number2 <= 0
+        puts "É impossível dividir por 0 ou por um valor negativo."
+    else
+        division = number1 / number2
+    puts "O resultado da soma é: #{division}"
+    end 
+else
+    puts "Não foi possível identificar qual operação você deseja realizar."
+end
