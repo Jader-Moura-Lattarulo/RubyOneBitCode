e04cc83dad3aee1aa42f6841aae7824dc57b23a5
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-02-14 13:56:08 -0300

    Inserção de loop

--- calculadora_simples.rb
+++ calculadora_simples.rb
@@ -1,33 +1,62 @@
-print "Digite o primeiro número inteiro: "
-number1 = gets.chomp.to_i
-print "Digite o segundo número inteiro: "
-number2 = gets.chomp.to_i
+puts "Bem-vindo(a) a calculadora"
+resposta_usuario = ""
 
-puts "\nEscolha uma operação:\n"
-puts "Soma\t\t->\t+"
-puts "Subitração\t->\t-"
-puts "Multiplicação\t->\t*"
-puts "Divisão\t\t->\t/"
+begin
+    
+    puts "O que você deseja fazer? "
+    puts "Fazer um calculo->\t1"
+    puts "Sair\t\t->\t2"
+    print "Digite sua opção: "
+    resposta_usuario = gets.chomp.to_i
 
-choice = gets.chomp
+        if resposta_usuario == 1
 
-case choice
-when '+'
-    addition = number1 + number2
-    puts "O resultado da soma é: #{addition}"
-when '-'
-    subtraction = number1 - number2
-    puts "O resultado da subtração é: #{subtraction}"
-when '*'
-    times = number1 * number2
-    puts "O resultado da multiplicação é: #{times}"
-when '/'
-    if number2 <= 0
-        puts "É impossível dividir por 0 ou por um valor negativo."
-    else
-        division = number1 / number2
-    puts "O resultado da soma é: #{division}"
-    end 
-else
-    puts "Não foi possível identificar qual operação você deseja realizar."
-end
+            system('cls') || system('clear')
+            puts "Você escolheu opção: #{resposta_usuario}"
+            print "\nDigite o primeiro número inteiro: "
+            number1 = gets.chomp.to_i
+            print "Digite o segundo número inteiro: "
+            number2 = gets.chomp.to_i
+
+            puts "\nEscolha uma operação:\n"
+            puts "Soma\t\t->\t+"
+            puts "Subitração\t->\t-"
+            puts "Multiplicação\t->\t*"
+            puts "Divisão\t\t->\t/"
+
+            choice = gets.chomp
+
+            case choice
+            when '+'
+                system('cls') || system('clear')
+                addition = number1 + number2
+                puts "O resultado da soma é: #{addition}"
+            when '-'
+                system('cls') || system('clear')
+                subtraction = number1 - number2
+                puts "O resultado da subtração é: #{subtraction}"
+            when '*'
+                system('cls') || system('clear')
+                times = number1 * number2
+                puts "O resultado da multiplicação é: #{times}"
+            when '/'
+                system('cls') || system('clear')
+                if number2 <= 0
+                    puts "É impossível dividir por 0 ou por um valor negativo."
+                else
+                    system('cls') || system('clear')
+                    division = number1 / number2
+                puts "O resultado da soma é: #{division}"
+                end 
+            else
+                system('cls') || system('clear')
+                puts "Não foi possível identificar qual operação você deseja realizar."
+            end
+
+        elsif resposta_usuario == 2
+            puts "Muito bem, até a próxima!"
+        
+        else
+            puts "Por favor, insira uma resposta válida:"
+        end
+end while resposta_usuario != 2
